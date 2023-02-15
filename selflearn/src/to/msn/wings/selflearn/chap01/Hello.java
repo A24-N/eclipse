0bfdf5cc1485e1ac4c38516b99536463565332ea
A24-N <117143210+A24-N@users.noreply.github.com>
2023-02-15 19:35:26 +0900

    [finish]chapter1

--- selflearn/src/to/msn/wings/selflearn/chap01/Hello.java
+++ selflearn/src/to/msn/wings/selflearn/chap01/Hello.java
@@ -1,10 +1,10 @@
 package to.msn.wings.selflearn.chap01;
 
+import java.time.LocalDateTime;
+
 public class Hello {
-
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		LocalDateTime time = LocalDateTime.now();
+		System.out.println(time);
 	}
-
 }
