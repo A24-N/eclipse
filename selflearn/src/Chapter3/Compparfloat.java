81d392215f0b708501f22ddf5a265d291efaabba
A24-N <117143210+A24-N@users.noreply.github.com>
2023-02-17 12:16:53 +0900

    [finish]list3_10~18

---
+++ selflearn/src/Chapter3/Compparfloat.java
@@ -0,0 +1,21 @@
+package Chapter3;
+
+import java.math.BigDecimal;
+
+public class Compparfloat {
+
+	public static void main(String[] args) {
+		System.out.println(0.2 * 3 == 0.6); //false
+		
+		var bd1 = new BigDecimal("0.2");
+		var bd2 = new BigDecimal("3");
+		var bd3 = new BigDecimal("0.6");
+		System.out.println(bd1.multiply(bd2).equals(bd3)); /**0 値が大きい場合は1　小さい場合は-1　同じ場合は0を表示する
+		equalsメソッドは有効桁数まで判定するので 1 = 1.0 →trueにはならない　**/
+		
+		final double EPSILON = 0.00001;
+		var x  = 0.2 * 3;
+		var y = 0.6;
+		System.out.println(Math.abs(x - y) < EPSILON); // true
+	}
+}
