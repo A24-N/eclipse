051d4ba9149c7a9ddd018542a834a4eef2a3561f
A24-N <117143210+A24-N@users.noreply.github.com>
2023-02-18 11:26:21 +0900

    [finish]practice4_1

---
+++ selflearn/src/Chapter4/Practice4_1.java
@@ -0,0 +1,18 @@
+package Chapter4;
+
+public class Practice4_1 {
+
+	public static void main(String[] args) {
+		var point = 75;
+		if (point > 90) {
+			System.out.println("優");
+		} else if (point > 70) {
+			System.out.println("良");
+		} else if (point > 50) {
+			System.out.println("可");
+		} else {
+			System.out.println("不可");
+		}
+	}
+
+}
