d1f4de026161dc1d44961fbc72353f51567da563
A24-N <117143210+A24-N@users.noreply.github.com>
2023-02-18 16:33:34 +0900

    [finish]list4_24~27

---
+++ selflearn/src/Chapter4/Break.java
@@ -0,0 +1,17 @@
+package Chapter4;
+
+public class Break {
+
+	public static void main(String[] args) {
+		int i;
+		int sum = 0;
+		
+		for (i = 1; i <= 1000; i++) {
+			sum += i;
+			if( sum > 10000) {
+				break;
+			}
+		}
+	System.out.println("合計値が10000を超えるのは、1～" + i + "を加算したときです。");
+	}
+}
---
+++ selflearn/src/Chapter4/BreakNest.java
@@ -0,0 +1,40 @@
+package Chapter4;
+
+public class BreakNest {
+
+	public static void main(String[] args) {
+		System.out.println("BreakNest1");
+		BreakNest1();
+		System.out.println();
+		System.out.println("BreakNest2");
+		BreakNest2();
+	}
+	
+	public static void BreakNest1() {
+		for (var i = 1; i < 10; i++) {
+			for (var j = 1; j < 10; j++) {
+				var result = i * j;
+				if (result > 50) {
+					break;
+				}
+				System.out.printf(" %2d", result);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void BreakNest2(){
+		limit:
+		for (var k = 1; k <10; k++) {
+			for (var l = 1; l < 10; l++) {
+				var result2 = k * l;
+				if (result2 > 50) {
+					break limit;
+				}
+				System.out.printf(" %2d", result2);
+			}
+			System.out.println();
+		}
+
+	}
+}
