5abddc9a614f2dd4f6f53fed631cbf6572b04c7b
A24-N <117143210+A24-N@users.noreply.github.com>
2023-02-19 12:05:57 +0900

    [finish]list5_1~9

--- selflearn/src/Chapter3/PracticeChapter3.java
+++ selflearn/src/Chapter3/PracticeChapter3.java
@@ -1,3 +1,4 @@
+package Chapter3;
 
 public class PracticeChapter3 {
 
---
+++ selflearn/src/Chapter5/Str1.java
@@ -0,0 +1,26 @@
+package Chapter5;
+
+public class Str1 {
+
+	public static void main(String[] args) {
+		System.out.println("StrLength");
+		StrLength();
+		System.out.println();
+		System.out.println("StrCodePoint");
+		StrCodePoint();
+	}
+	
+	public static void StrLength() {
+		var str1 = "こんにちわ、さようなら";
+			System.out.println(str1.length()); //11
+			
+//			サロゲートペアを識別できない為、4バイト=2文字としてカウントしている。
+		var str2 = "𪛊𪛊";
+			System.out.println(str2.length()); //4
+	}
+	
+	public static void StrCodePoint() {
+		var str = "𪛊𪛊";
+			System.out.println(str.codePointCount(0, str.length())); //2
+	}
+}
