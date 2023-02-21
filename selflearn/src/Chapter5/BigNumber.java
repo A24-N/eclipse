a79e71a7864f1c1615c537cf998b9a054f71f458
A24-N <117143210+A24-N@users.noreply.github.com>
2023-02-21 12:47:11 +0900

    [finish]list5_51~53

---
+++ selflearn/src/Chapter5/BigNumber.java
@@ -0,0 +1,31 @@
+package Chapter5;
+
+import java.math.BigInteger;
+
+public class BigNumber {
+
+	public static void main(String[] args) {
+		System.out.println("bigNumber");
+		bigNumber();
+		System.out.println();
+		System.out.println("bigNumber2");
+		bigNumber2();
+	}
+	
+	public static void bigNumber() {
+		long result = 1;
+		for (var i = 1; i < 26; i++) {
+			result *= i;
+			System.out.println(result);
+		}
+	}
+	
+	public static void bigNumber2() {
+		var result = BigInteger.valueOf(1);
+		for (var i = 1; i < 26; i++) {
+			result =result.multiply(BigInteger.valueOf(i));
+			System.out.println(result);
+		}
+	}
+
+}
---
+++ selflearn/src/Chapter5/MathEx.java
@@ -0,0 +1,22 @@
+package Chapter5;
+
+public class MathEx {
+
+	public static void main(String[] args) {
+		System.out.println(Math.abs(-56));						//絶対値
+		System.out.println(Math.min(3, 98));					//最小値
+		System.out.println(Math.max(3, 98));					//最大値
+		System.out.println(Math.ceil(1233.97));					//切り上げ
+		System.out.println(Math.floor(1234.99));				//切り捨て
+		System.out.println(Math.round(1233.65));				//四捨五入
+		System.out.println(Math.sqrt(4));						//平方根
+		System.out.println(Math.cbrt(4));						//立方根
+		System.out.println(Math.pow(2,5));						//xのy乗
+		System.out.println(Math.sin(Math.toRadians(30)));		//サイン
+		System.out.println(Math.cos(Math.toRadians(60)));		//コサイン
+		System.out.println(Math.tan(Math.toRadians(45)));		//タンジェント
+		System.out.println(Math.log(100));						//自然対数(底eの対数)
+		System.out.println(Math.log10(100));					//底10の対数
+	}
+
+}
