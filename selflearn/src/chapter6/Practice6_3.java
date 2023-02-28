67857e6b39bceafa6e825b195c422854b400fd47
A24-N <117143210+A24-N@users.noreply.github.com>
2023-02-28 13:56:55 +0900

    [finish][selfstudy_java]practice6_3

---
+++ selflearn/src/chapter6/Practice6_3.java
@@ -0,0 +1,13 @@
+package chapter6;
+
+public class Practice6_3 {
+
+	public static void main(String[] args) {
+		System.out.print("リストと違い重複を許可しない。\n"
+				+ "数学の集合に似ており、ある要素が含まれているかなどの包含関係に利用することができる。\n"
+				+ "HashSetとTreeSetというクラスがあり、HashSetは並び順を管理していないが、\n"
+				+ "TreeSetはソート機能が備えられており、大小比較を主としたメソッドが用意されている。");
+
+	}
+
+}
