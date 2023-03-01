5629bced7fdd9677bea299d4bf461162aaa22592
A24-N <117143210+A24-N@users.noreply.github.com>
2023-03-01 17:24:20 +0900

    [finish][selfstudy_java]list7_11~16

---
+++ selflearn/src/chapter7/SvopeBlock.java
@@ -0,0 +1,32 @@
+package chapter7;
+
+public class SvopeBlock {
+
+	public static void main(String[] args) {
+		var data = "ローカルスコープ";
+		{
+//			var data = "ローカルスコープ"; エラー：重複ローカル変数 data
+		}
+		System.out.println(data);
+		
+		
+//		変数dataを宣言する順序を替えるとエラーは発生しない。
+		{
+			var data1 = "ブロックスコープ";
+			System.out.println(data1);
+		}
+		
+		var data1 = "ローカルスコープ";
+		System.out.println(data1);
+		
+		{
+			var data2 = "ブロックスコープ1";
+			System.out.println(data2);
+		}
+		{
+			var data2 = "ブロックスコープ2";
+			System.out.println(data2);
+		}
+	}
+	
+}
