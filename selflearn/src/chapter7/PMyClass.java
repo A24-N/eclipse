8ccf198505760ebb7c040759026b19e9b10db937
A24-N <117143210+A24-N@users.noreply.github.com>
2023-03-02 14:43:53 +0900

    [finish][selfstudy_java]practice7_3

---
+++ selflearn/src/chapter7/PMyClass.java
@@ -0,0 +1,9 @@
+package chapter7;
+
+public class PMyClass {
+
+	public static double getBmi(Double weight, Double height) {
+		return weight / (height * height);
+	}
+
+}
---
+++ selflearn/src/chapter7/Practice7_3.java
@@ -0,0 +1,8 @@
+package chapter7;
+
+public class Practice7_3 {
+
+	public static void main(String[] args) {
+		System.out.println(PMyClass.getBmi(60.0, 160.0));
+	}
+}
