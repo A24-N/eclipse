f8ef9f7892429883db5d25ed109b4ebb07517f56
A24-N <117143210+A24-N@users.noreply.github.com>
2023-03-05 15:04:55 +0900

    [finish][selfstudy_java]practice9_1

---
+++ selflearn/src/chapter9/practice9/Practice1.java
@@ -0,0 +1,22 @@
+package chapter9.practice9;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		System.out.println("問1");
+		q1();
+		System.out.println("");
+
+	}
+	
+	public static void q1() {
+		System.out.println("①：Object");
+		System.out.println("②：this");
+		System.out.println("③：instanceof");
+		System.out.println("④：(Person)");
+		System.out.println("⑤：equals");
+		System.out.println("⑥：false");
+
+	}
+
+}
