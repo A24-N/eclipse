ac42c6f7874eaa103410cb566e252ac92b34883d
A24-N <117143210+A24-N@users.noreply.github.com>
2023-03-06 17:49:14 +0900

    [finish][selfstudy_java]list9_24~25

--- selflearn/src/chapter9/MySampleException.java
+++ selflearn/src/chapter9/MySampleException.java
@@ -6,4 +6,20 @@ public class MySampleException extends Exception {
 		// TODO 自動生成されたコンストラクター・スタブ
 	}
 
+	public MySampleException() {
+		super();
+	}
+	
+	public MySampleException(String message) {
+		super(message);
+	}
+	
+	public MySampleException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public MySampleException(Throwable cause) {
+		super(cause);
+	}
+
 }
