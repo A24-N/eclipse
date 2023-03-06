23ccdd7b3da3d68c5034bb193e1d6ed889949eef
A24-N <117143210+A24-N@users.noreply.github.com>
2023-03-06 19:18:21 +0900

    [finish][selfstudy_java]practice9_3

---
+++ selflearn/src/chapter9/practice9/Practice3.java
@@ -0,0 +1,12 @@
+package chapter9.practice9;
+
+public class Practice3 {
+
+	public static void main(String[] args) {
+		for (var we: Weekday.values()) {
+			System.out.println(we.ordinal() + ":" + we.toString());
+		}
+
+	}
+
+}
---
+++ selflearn/src/chapter9/practice9/Weekday.java
@@ -0,0 +1,11 @@
+package chapter9.practice9;
+
+public enum Weekday {
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY;
+}
