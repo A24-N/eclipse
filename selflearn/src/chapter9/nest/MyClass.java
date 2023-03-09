f607833116990370af61a389f0cbd7b336520188
A24-N <117143210+A24-N@users.noreply.github.com>
2023-03-09 16:42:18 +0900

    [finish][selfstudy_java]list9_37~40

---
+++ selflearn/src/chapter9/nest/MyClass.java
@@ -0,0 +1,26 @@
+package chapter9.nest;
+
+class MyClass {
+	private String str1 = "包含・インスタンス";
+	private static String str2 = "包含・クラス";
+	
+//	内部クラスの定義
+	private class MyHelper {
+		private String str1 = "ネスト・インスタンス";
+		private static final String str2 = "ネスト・クラス";
+		
+		public void show() {
+			System.out.println(MyClass.this.str1);
+			System.out.println(MyClass.str2);
+		}
+	}
+	
+	public void run() {
+		var helper = new MyHelper();
+		helper.show();
+		System.out.println(helper.str1);
+		System.out.println(MyHelper.str2);
+		
+	}
+
+}
---
+++ selflearn/src/chapter9/nest/NestedAccess.java
@@ -0,0 +1,11 @@
+package chapter9.nest;
+
+public class NestedAccess {
+
+	public static void main(String[] args) {
+		var c = new MyClass();
+		c.run();
+
+	}
+
+}
