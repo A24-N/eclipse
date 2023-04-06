ffa2c529a83fdcfb6b1ee46312a7e40c1ea5567b
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-06 16:58:49 +0900

    Java1_9

---
+++ progate/src/progate/Java1_9_main.java
@@ -0,0 +1,9 @@
+package progate;
+
+public class Java1_9_main {
+	
+	public static void main(String[] args) {
+		Java1_9_person.hello();
+	}
+
+}
---
+++ progate/src/progate/Java1_9_person.java
@@ -0,0 +1,9 @@
+package progate;
+
+public class Java1_9_person {
+	
+	public static void hello() {
+		System.out.println("Hello Java");
+	}
+
+}
