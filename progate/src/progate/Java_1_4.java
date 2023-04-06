55336f71e5515be4d0b395c006ce841dab1f6643
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-06 14:53:53 +0900

    Java_1_4.java

---
+++ progate/src/progate/Java_1_4.java
@@ -0,0 +1,15 @@
+package progate;
+
+public class Java_1_4 {
+	
+	public static void main (String[] args) {
+		printData("田中",30);
+		printData("山田",5);
+	}
+	
+	public static void printData(String name,int age) {
+		System.out.println("私の名前は" + name + "です");
+		System.out.println("年齢は" + age + "歳です");
+		
+	}
+}
