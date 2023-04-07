81a8cf8082050da0a2a227ac8b3c6196591df65b
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-07 17:21:14 +0900

    java4-5

--- progate/src/progate/java4/Main_3.java
+++ progate/src/progate/java4/Main_3.java
@@ -4,8 +4,15 @@ public class Main_3 {
 	
 	public static void main(String[] args) {
 		Person_3 person1 = new Person_3();
-		Person_3 person2 = new Person_3();
+		person1.hello();
+		person1.name = "Kate Jones";
+		System.out.println(person1.name);
 		
+		
+		Person_3 person2 = new Person_3();
+		person2.hello();
+		person2.name = "John Christopher Smith";
+		System.out.println(person2.name);
 	}
 
 }
--- progate/src/progate/java4/Person_3.java
+++ progate/src/progate/java4/Person_3.java
@@ -1,5 +1,11 @@
 package progate.java4;
 
 public class Person_3 {
+	
+	public String name;
+	
+	public void hello() {
+		System.out.println("こんにちは");
+	}
 
 }
