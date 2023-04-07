ab8627466bad82736dc750209ffb71385f709122
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-07 14:28:47 +0900

    practice1-3

--- progate/src/progate/practice1/Main.java
+++ progate/src/progate/practice1/Main.java
@@ -1,9 +1,16 @@
 package progate.practice1;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("名前はKate Jonesです");
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("名前：");
+		String firstName = scanner.next();
+		System.out.println("名前は" + firstName + "です");
+		
 	}
 
 }
