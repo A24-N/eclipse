fe9c129e173b0ef74a3bba1ee05d7901906806ee
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-07 15:19:44 +0900

    practice1-7

--- progate/src/progate/practice1/Main.java
+++ progate/src/progate/practice1/Main.java
@@ -17,25 +17,8 @@ public class Main {
 		double height = scanner.nextDouble();
 		System.out.print("体重(kg)");
 		double weight = scanner.nextDouble();
-		String fullName = fullName(firstName, lastName);
-		printData(fullName, age, height, weight);
+		Person.printData(firstName, lastName, age, height, weight);
 		
-	}
-
-	public static void printData(String fullName, int age, double height, double weight) {
-		System.out.println("名前は" + fullName + "です");
-		System.out.println("年齢は" + age + "歳です");
-		if (age >= 20) {
-			System.out.println("成年者です");
-		} else {
-			System.out.println("未成年者です");
-		}
-		System.out.println("身長は" + height + "mです");
-		System.out.println("体重は" + weight + "kgです");
-	}
-	
-	private static String fullName(String firstName, String lastName) {
-		return firstName + " " + lastName;
 	}
 
 }
