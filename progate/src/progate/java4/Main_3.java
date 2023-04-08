a2628cfccc57e232bdb291be7e6cd93e894dec6c
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-08 11:12:43 +0900

    java4-9

--- progate/src/progate/java4/Main_3.java
+++ progate/src/progate/java4/Main_3.java
@@ -3,13 +3,11 @@ package progate.java4;
 public class Main_3 {
 	
 	public static void main(String[] args) {
-		Person_3 person1 = new Person_3();
-//		person1.name = "Kate Jones";
-//		person1.hello();
+		Person_3 person1 = new Person_3("Kate Jones");
+		person1.hello();
 		
-		Person_3 person2 = new Person_3();
-//		person2.name = "John Christopher Smith";
-//		person2.hello();
+		Person_3 person2 = new Person_3("John Christopher Smith");
+		person2.hello();
 	}
 
 }
--- progate/src/progate/java4/Person_3.java
+++ progate/src/progate/java4/Person_3.java
@@ -4,8 +4,9 @@ public class Person_3 {
 	
 	public String name;
 	
-	public Person_3() {
+	public Person_3(String name) {
 		System.out.println("インスタンスが生成されました");
+		this.name = name;
 	}
 	
 	public void hello() {
