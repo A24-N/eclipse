cf7e79c1ac4a5637f0e85ceaa49da2034d356795
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-08 10:51:40 +0900

    java4-7

--- progate/src/progate/java4/Main_3.java
+++ progate/src/progate/java4/Main_3.java
@@ -4,15 +4,12 @@ public class Main_3 {
 	
 	public static void main(String[] args) {
 		Person_3 person1 = new Person_3();
-		person1.hello();
-		person1.name = "Kate Jones";
-		System.out.println(person1.name);
-		
+//		person1.name = "Kate Jones";
+//		person1.hello();
 		
 		Person_3 person2 = new Person_3();
-		person2.hello();
-		person2.name = "John Christopher Smith";
-		System.out.println(person2.name);
+//		person2.name = "John Christopher Smith";
+//		person2.hello();
 	}
 
 }
--- progate/src/progate/java4/Person_3.java
+++ progate/src/progate/java4/Person_3.java
@@ -4,8 +4,12 @@ public class Person_3 {
 	
 	public String name;
 	
+	public Person_3() {
+		System.out.println("インスタンスが生成されました");
+	}
+	
 	public void hello() {
-		System.out.println("こんにちは");
+		System.out.println("こんにちは、私は" + this.name + "です");
 	}
 
 }
