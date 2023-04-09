eb8b08b1dfa2cc5736fc7ad0ab561a2a1720205b
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-09 13:52:10 +0900

    practice2-4

--- progate/src/progate/practice2/Bycycle.java
+++ progate/src/progate/practice2/Bycycle.java
@@ -3,13 +3,24 @@ package progate.practice2;
 class Bycycle {
 	
 	private String name;
+	private String color;
+	private int distance = 0;
 	
-	public Bycycle(String name) {
+	public Bycycle(String name, String color, int distance) {
 		this.name = name;
+		this.color = color;
+		this.distance = distance;
 	}
 	
 	public String getBycycleName() {
 		return this.name;
+	}
+	
+	public void printData() {
+		System.out.println("【自転車の情報】");
+		System.out.println("名前：" + this.name);
+		System.out.println("色：" + this.color);
+		System.out.println("走行距離：" + this.distance +"km");
 	}
 
 }
--- progate/src/progate/practice2/Main.java
+++ progate/src/progate/practice2/Main.java
@@ -4,9 +4,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 	
-	Bycycle bycycle1 = new Bycycle("ビアンキ");
-	System.out.println("【自転車の情報】");
-	System.out.println("名前：" + bycycle1.getBycycleName());
+	Bycycle bycycle1 = new Bycycle("ビアンキ", "緑", 15);
+	bycycle1.printData();
+
 	
 	}
 }
