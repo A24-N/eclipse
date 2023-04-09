4302a5d09f559a728752aadbf8c483eecd161b39
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-09 11:50:33 +0900

    practice2-2

---
+++ progate/src/progate/practice2/Bycycle.java
@@ -0,0 +1,15 @@
+package progate.practice2;
+
+class Bycycle {
+	
+	private String name;
+	
+	public Bycycle(String name) {
+		this.name = name;
+	}
+	
+	public String getBycycleName() {
+		return this.name;
+	}
+
+}
---
+++ progate/src/progate/practice2/Main.java
@@ -0,0 +1,12 @@
+package progate.practice2;
+
+public class Main {
+	
+	public static void main(String[] args) {
+	
+	Bycycle bycycle1 = new Bycycle("ビアンキ");
+	System.out.println("【自転車の情報】");
+	System.out.println("名前：" + bycycle1.getBycycleName());
+	
+	}
+}
