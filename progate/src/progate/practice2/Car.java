d437b3ff17fccc6b11f234c06666cc76b74467a5
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-09 14:14:15 +0900

    practice2-7

--- progate/src/progate/practice2/Car.java
+++ progate/src/progate/practice2/Car.java
@@ -1,12 +1,12 @@
 package progate.practice2;
 
-class Bycycle {
+class Car {
 	
 	private String name;
 	private String color;
 	private int distance = 0;
 	
-	public Bycycle(String name, String color, int distance) {
+	public Car(String name, String color, int distance) {
 		this.name = name;
 		this.color = color;
 		this.distance = distance;
@@ -17,15 +17,14 @@ class Bycycle {
 	}
 	
 	public void printData() {
-		System.out.println("【自転車の情報】");
 		System.out.println("名前：" + this.name);
 		System.out.println("色：" + this.color);
-		System.out.println("走行距離：" + this.distance +"km");
+//		System.out.println("走行距離：" + this.distance +"km");
 	}
 	
-	public void run(int bycycleDistance) {
-		this.distance=+ bycycleDistance;
-		System.out.println( bycycleDistance + "km走ります");
+	public void run(int carDistance) {
+		this.distance=+ carDistance;
+		System.out.println( carDistance + "km走ります");
 		System.out.println("走行距離：" + this.distance + "km");
 	}
 
--- progate/src/progate/practice2/Main.java
+++ progate/src/progate/practice2/Main.java
@@ -1,12 +1,23 @@
 package progate.practice2;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
 	
-	Bycycle bycycle1 = new Bycycle("ビアンキ", "緑", 15);
+	Bycycle bycycle1 = new Bycycle("ビアンキ", "緑", 0);
+	System.out.println("【自転車の情報】");
 	bycycle1.printData();
-
+	System.out.println("-----------------");
+	Scanner scanner = new Scanner(System.in);
+	System.out.print("走る距離を入力してください：");
+	int bycycleDistance = scanner.nextInt();
+	bycycle1.run(bycycleDistance);
+	System.out.println("=================");
+	Car car1 = new Car("フェラーリ","赤", 0);
+	System.out.println("【車の情報】");
+	car1.printData();
 	
 	}
 }
