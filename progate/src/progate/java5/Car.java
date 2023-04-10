f7d95d0faccb36ff6f1db6cccc5eb971d5cff3b7
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-10 14:31:52 +0900

    java5-6

--- progate/src/progate/java5/Car.java
+++ progate/src/progate/java5/Car.java
@@ -1,5 +1,29 @@
 package progate.java5;
 
 public class Car extends Vehicle {
+	
+	private int fuel = 50;
+	
+	public int getFuel() {
+		return this.fuel;
+	}
+	
+	public void charge(int litre) {
+		System.out.println(litre + "L給油します");
+		if (litre <= 0) {
+			System.out.println("給油できません");
+		} else if (litre + this.fuel >= 100) {
+			System.out.println("満タンまで給油します");
+			this.fuel = 100;
+		} else {
+			this.fuel += litre;
+		}
+		System.out.println(this.fuel);
+	}
+	
+	public void printData() {
+		super.printData();
+    System.out.println("ガソリン量：" + this.fuel + "L");
+	}
 
 }
