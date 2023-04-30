ca1b02cbd010b47e33ae59b7fe28272aa10b1c60
A24-N <117143210+A24-N@users.noreply.github.com>
2023-04-30 11:09:07 +0900

    test

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,17 +4,20 @@ import java.util.Scanner;
 
 public class Test {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    
-    // 入力
-    long N = sc.nextLong();
-    // 出力
-    for (long i = 1; i * i <= N; i++) {
-    	if (N % i == 0) {
-    		System.out.println(i);
-    		if (i != N / i) System.out.println(N / i);
-    	}
-    }
-  }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// 入力
+		long N = sc.nextLong();
+		// 出力
+		for (long i = 2; i * i <= N; i++) {
+			if (N % i == 0) {
+				N /= i;
+				System.out.print(i + " ");
+				i = 1;
+			}
+		}
+		System.out.println(N);
+		
+	}
 }
